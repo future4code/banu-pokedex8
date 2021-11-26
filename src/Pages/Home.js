9de8f1949ce37e645aa221e8f89b67f91ef3f797
midoriobana <ingridmidori@live.com>
2021-11-25 22:42:19 -0300

    save

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,26 +7,15 @@ import PokeCard from "../Components/PokeCard";
 const Home = () =>{
     const [pokemon, setPokemon] = useState([])
     const [pokemonData, setPokemonData] = useState([])
-    const [urls, setUrls] = useState([])
 
     const getPokemons = () => {
-        const pokeList = urls
-        const getUrls = () => {
-            pokemonData.map((item) => {
-                pokeList.push(item.url)
-                setUrls(pokeList)
-            })
-        }
         axios
         .get(`${BASE_URL}/pokemon?limit=30&offset=200`)
         .then((res) => {
             setPokemonData(res.data.results)
-            console.log(urls)
         }).catch((err) => {
             console.log(err)
         })
-
-        getUrls()
     }
 
     useEffect(() => {
